b52c4ad270a6f7252a8812ac2ceba73e6b9c6f56
Gabriel Chaves <78745759+gabrielchavesTW@users.noreply.github.com>
2021-02-09 09:45:19 -0300

    show menu of options and read user selected option

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,15 @@
 package com.twu.biblioteca;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public class BibliotecaApp {
     private PrintStream printStream;
     private BufferedReader bufferedReader;
-    public String WELCOME_MESSAGE = "";
+    public static final String WELCOME_MESSAGE = "";
+    public static final String MENU = "1- List of books";
+    public static final String INVALID_OPTION_MESSAGE = "Invalid option.";
 
     public BibliotecaApp(PrintStream printStream, BufferedReader bufferedReader){
         this.printStream = printStream;
@@ -15,5 +18,23 @@ public class BibliotecaApp {
 
     public void showWelcomeMessage(){
         printStream.println(WELCOME_MESSAGE);
+    }
+
+    public void showMenu() throws IOException {
+        printStream.println(MENU);
+        readMenuOption();
+    }
+
+    public void readMenuOption() throws IOException {
+        String menuOption = bufferedReader.readLine();
+        showSelectedMenuOption(menuOption);
+    }
+
+    private void showSelectedMenuOption(String menuOption) throws IOException {
+        if(menuOption == "1")
+            printStream.println("Show list of books");
+        else {
+            printStream.println(INVALID_OPTION_MESSAGE);
+        }
     }
 }
--- src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
+++ src/test/java/com/twu/biblioteca/BibliotecaAppTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+
+import static org.mockito.Mockito.*;
 
 public class BibliotecaAppTest {
      PrintStream printStream;
@@ -21,8 +22,28 @@ public class BibliotecaAppTest {
     }
 
     @Test
-    public void showWelcomeMessage() {
+    public void shouldShowWelcomeMessage() {
         biblioteca.showWelcomeMessage();
         verify(printStream).println(biblioteca.WELCOME_MESSAGE);
+    }
+
+    @Test
+    public void shouldShowMenuOfOptions() throws IOException {
+        biblioteca.showMenu();
+        verify(printStream).println(biblioteca.MENU);
+    }
+
+    @Test
+    public void shouldReturnListOfBooksWhenOptionSelectedIsOne() throws IOException {
+        when(bufferedReader.readLine()).thenReturn("1");
+        biblioteca.readMenuOption();
+        verify(printStream).println("Show list of books");
+    }
+
+    @Test
+    public void shouldShowInvalidOption() throws IOException {
+        when(bufferedReader.readLine()).thenReturn("132");
+        biblioteca.readMenuOption();
+        verify(printStream).println(biblioteca.INVALID_OPTION_MESSAGE);
     }
 }
